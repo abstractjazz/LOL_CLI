943503013d741666cedf86b43c3b8574334348b1
Evan Burton <evan@heyevan.com>
2020-07-05 20:05:30 +0000

    stubbing functionality in controller file

--- LOL_CLISUB/lib/LOL_CLI/CLICLASS.rb
+++ LOL_CLISUB/lib/LOL_CLI/CLICLASS.rb
@@ -1,19 +1,23 @@
 #CLI CONTROLLER (responsible for user interactions) 
 
-
 class LOL_CLI::CLI 
 
 def call 
-
-puts "I just flew in from Albuquerque, and boy are my params tired."
-
-sleep 3 
-
-puts "But enough about me."
-
-sleep 2.2
-
-puts "How many jokes would you like to see? Pick a number from 1-75."
-
+  puts "I just flew in from Albuquerque, and boy are my params tired."
+  sleep 3 
+  puts "But enough about me."
+  sleep 2.2
+  get_jokes
+end 
+  
+def get_jokes
+  puts "How many jokes would you like to see? Pick a number from 1-75."
+  input = gets.strip 
+  if input == (1..75)
+  puts input 
+  else 
+  puts "Is this thing on? Pick a number from 1-75."
+  
+    end 
   end 
 end 
