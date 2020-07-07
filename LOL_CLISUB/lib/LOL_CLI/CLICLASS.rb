da8aee86c468961bbbe546d38d0f80434f0e0544
Evan Burton <evan@heyevan.com>
2020-07-07 02:01:27 +0000

    began writing jokes class

--- LOL_CLISUB/lib/LOL_CLI/CLICLASS.rb
+++ LOL_CLISUB/lib/LOL_CLI/CLICLASS.rb
@@ -7,17 +7,41 @@ def call
   sleep 3 
   puts "But enough about me."
   sleep 2.2
-  get_jokes
+  menu
 end 
   
-def get_jokes
-  puts "How many jokes would you like to see? Pick a number from 1-75."
-  input = gets.strip 
-  if input == (1..75)
-  puts input 
-  else 
-  puts "Is this thing on? Pick a number from 1-75."
+def get_jokes 
   
+  @jokes = LOL_CLI::RD_jokes.all
+  
+  
+ def goodbye
+  puts "It's been a pleasure. Be sure to tip your programmer."
+  end 
+
+def more_jokes
+  puts "I'm here all night. Do you want more jokes? [y/n]" 
+  input = gets.strip
+  case input 
+  when "n"
+    goodbye 
+  when "y"
+    menu
+    end 
+  end 
+
+def menu
+  puts "Pick a number from 1-75."
+  input = gets.strip 
+  if input.to_i > 75 || input.to_i < 1
+  puts "Is this thing on?"
+  menu
+  else 
+  get_jokes 
+  sleep 4.2 
+  more_jokes
     end 
   end 
 end 
+
+  
---
+++ LOL_CLISUB/lib/LOL_CLI/JOKESCLASS.rb
@@ -0,0 +1,12 @@
+class LOL_CLI::RD_jokes
+  
+def self.all 
+
+#returns a bunch of instances of jokes 
+
+  puts "What's the best thing about Switzerland?" 
+  sleep 2.2
+  puts "I don't know, but the flag is a big plus."  
+ end 
+  end
+end 
