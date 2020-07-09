e2fc55da94a119c5affe086ed6f42b359926ce1a
Evan Burton <evan@heyevan.com>
2020-07-09 17:50:26 +0000

    built scaper method

--- LOL_CLISUB/lib/LOL_CLI/JOKESCLASS.rb
+++ LOL_CLISUB/lib/LOL_CLI/JOKESCLASS.rb
@@ -14,28 +14,34 @@ def self.scrape_deals
 deals = [ ]
 
 
-deals 
-
+deals << self.scrape_jokes 
 
 #go to RD, find the joke_1
 #extract the properties 
 #instatiate a joke 
 
-joke_1 = self.new 
- joke_1.name = "Switzerland"
- joke_1.setup = "What's the best thing about Switzerland?"
- joke_1.punchline = "I don't know, but the flag is a big plus."
- joke_1.url = "https://www.rd.com/list/short-jokes/"
+deals 
 
- joke_2 = self.new 
- joke_2.name = "Invention"
- joke_2.setup = "I invented a new word!"
- joke_2.punchline = "Plagiarism!"
- joke_2.url = "https://www.rd.com/list/short-jokes/"
+# joke_1 = self.new 
+# joke_1.name = "Switzerland"
+# joke_1.setup = "What's the best thing about Switzerland?"
+# joke_1.punchline = "I don't know, but the flag is a big plus."
+# joke_1.url = "https://www.rd.com/list/short-jokes/"
 
-[joke_1, joke_2]  
+# joke_2 = self.new 
+# joke_2.name = "Invention"
+# joke_2.setup = "I invented a new word!"
+# joke_2.punchline = "Plagiarism!"
+# joke_2.url = "https://www.rd.com/list/short-jokes/"
+
+# [joke_1, joke_2]  
 
   end 
+  
+  def self.scrape_jokes
+    doc = Nokogiri::HTML(open("https://www.rd.com/list/short-jokes/"))  
+  
+  
 end 
 
 #returns instances of jokes 
